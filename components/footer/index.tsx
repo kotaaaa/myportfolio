bd8176db6799f1bf7f47bf9146671279cfb023c2
kotaaaa <kota.k.1132.pda@gmail.com>
2024-01-17 17:08:50 -0800

    fix: refactored

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -12,22 +12,22 @@ const Footer = () => {
           {/* Contact & SNS */}
           <ul className="flex gap-5 justify-end">
             <li>
-              <a href="mailto:kota.k.1132.pda@gmail.com" className="hover:underline">
+              <a href="mailto:kota.k.1132.pda@gmail.com" className="">
                 <Image src="/logos/mail.svg" width={25} height={25} alt="mail" />
               </a>
             </li>
             <li>
-              <a href="https://twitter.com/kotaaaa1110" className="hover:underline">
+              <a href="https://twitter.com/kotaaaa1110" className="">
                 <Image src="/logos/x.svg" width={25} height={25} alt="x" />
               </a>
             </li>
             <li>
-              <a href="https://github.com/kotaaaa" className="hover:underline">
+              <a href="https://github.com/kotaaaa" className="">
                 <Image src="/logos/github.svg" width={25} height={25} alt="github" />
               </a>
             </li>
             <li>
-              <a href="https://www.linkedin.com/in/kota1110/" className="hover:underline">
+              <a href="https://www.linkedin.com/in/kota1110/" className="">
                 <Image src="/logos/linkedin.svg" width={25} height={25} alt="linkedin" />
               </a>
             </li>
